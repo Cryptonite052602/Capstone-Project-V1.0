4be131dfa16128d22b02712abb4333e1b8f81b04
Archiel R. Cabanag <Capstone Project Developin V1.0>
2025-12-18 00:48:03 +0800

    Add

--- index-admin-staff.php
+++ index-admin-staff.php
@@ -34,7 +34,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Barangay Luz Healthcare</title>
+    <title>Login | Barangay Luz, Cebu City</title>
+    <link rel="icon" type="image/png" href="./asssets/images/Luz.jpg">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
